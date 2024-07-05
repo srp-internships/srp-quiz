2954927483ffc04c24ceb731142923b9bbe8c947
Abbos Sidiqov <abbosidiqov@outlook.com>
2024-07-05 08:55:34 +0500

    remove duplicate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -24,18 +24,5 @@ export const appConfig: ApplicationConfig = {
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideFunctions(() => getFunctions()),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'srp-quiz',
-        appId: '1:623714979122:web:985bd02fed586554541b2b',
-        storageBucket: 'srp-quiz.appspot.com',
-        apiKey: 'AIzaSyAemyRjxpQoI0qL6YrWkdc_JQnUdOoygxQ',
-        authDomain: 'srp-quiz.firebaseapp.com',
-        messagingSenderId: '623714979122',
-      })
-    ),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideFunctions(() => getFunctions()),
   ],
 };
