b3f9226f4203a7deb21c4bc733e4c723dd3f170d
Улугбек Алимов <alimovu966@gmail.com>
2024-07-24 11:19:28 +0500

    hiding navbar in login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'srp-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'srp-quiz';
+  showNavbar: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showNavbar = !this.router.url.includes('/login');
+      }
+    });
+  }
 }
